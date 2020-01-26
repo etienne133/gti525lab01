a3043b78dd16de14f869471df9d4747eef902fae
LoicFettich <loicfettich@gmail.com>
2020-01-26 11:42:46 -0500

    reduce with map

--- src/Container.tsx
+++ src/Container.tsx
@@ -8,6 +8,14 @@ import App from './App'
 import IAppState from './App'
 //ICI ON MANAGE LE STATE DE L'APP ... 
 
+export enum CategoryTypes  {
+  LOCAL = 'local',
+  SHUTTLE_OR = 'shuttleOr',
+  NIGHT = 'night',
+  EXPRESS = 'express',
+  DEDICATED = 'dedicated'
+}
+
 export interface IAppState {
   lines : line[]; 
   handleLineClick : (id:string) => void 
@@ -40,34 +48,12 @@ const reducer = (accumulator:string[], currentValue:string) => accumulator.push(
 
 class Container extends React.Component<any>
 {
-  constructor(props:any) {
+  constructor(props: any) {
     super(props);
-    const line:line[] = linesFile.map(x=>x)
-    // const categories:category[] = linesFile.map<category>(
-    //   (key)=>({
-    //     name:key.name
-        
-    // }))
-    const categories2:category[] = linesFile.reduce<any>(
-      (acc, curr)=>{
-        if(!acc[curr.category]) acc[curr.category] = []; //If this type wasn't previously stored
-        acc[curr.category].push(curr);
-        return acc;
-    },{})
-    console.log(categories2);  
-     //retursn
-    //console.log(line)
-    //console.log(categories)
-    // const abc:IAppState =  { 
-    //   arrivals:arrivals, 
-    //   lines:lines,
-    //   stops:stops,
-    // };
-    //lecture des fichier et mapping ici. 
-    
-
-    this.setState(line)
-    //this.handleClick = this.handleClick.bind(this);
+    this.state = {
+      line: [],
+      lineMap : new Map<string, category>()
+    }
   }
 
   handleClick = (e:React.MouseEvent) => {
@@ -79,7 +65,43 @@ class Container extends React.Component<any>
 
   }
   componentDidMount() {
- 
+    const line:line[] = linesFile.map(x=>x)
+    // const categories:category[] = linesFile.map<category>(
+    //   (key)=>({
+    //     name:key.name
+        
+    // }))
+    const categories2:category[] = linesFile.reduce<any>(
+      (acc, curr)=>{
+        if(!acc[curr.category]) acc[curr.category] = []; //If this type wasn't previously stored
+        acc[curr.category].push(curr);
+        return acc;
+    },{});
+    console.log('CATEGORIES v2', categories2);  
+
+    const categories3: Map<string, category[]> = linesFile.reduce<any>(
+      (map: Map<string, category[]>, current: any) => {
+        if (!map.has(current.category)) map.set(current.category, []);
+        const array = map.get(current.category) as category[];
+        array.push(current);
+        return map.set(current.category, array);
+      }, new Map<string, category[]>());
+
+    Object.values(CategoryTypes).forEach(x => console.log('CATEGORIES v3 -', `${x}:`, categories3.get(x)));
+
+     //retursn
+    //console.log(line)
+    //console.log(categories)
+    // const abc:IAppState =  { 
+    //   arrivals:arrivals, 
+    //   lines:lines,
+    //   stops:stops,
+    // };
+    //lecture des fichier et mapping ici. 
+    
+
+    this.setState({line, lineMap: categories3});
+    //this.handleClick = this.handleClick.bind(this);
   }
 
 
