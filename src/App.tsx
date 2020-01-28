c189340ec4fad43a6c2da37f5954fe28381fa088
LoicFettich <loicfettich@gmail.com>
2020-01-27 19:51:59 -0500

    dynamic colors

--- src/App.tsx
+++ src/App.tsx
@@ -17,9 +17,6 @@ const APP:FC<patate> = ({handleClick, map}) =>{
 
     const [showLine, setLine] = useState('');
 
-    useEffect(() => {
-    });
-
     return (
         <div className="App">
           <div className="wrapper">
@@ -33,15 +30,15 @@ const APP:FC<patate> = ({handleClick, map}) =>{
                 {/* TODO: dynamically fill <ul/>'s */}
                 {/* <span className="list-el" onClick={() => toggleMenu(!showAll)}>Toutes les lignes</span>
                 <ul>{all?.map((x, index) => {return <li key={index} className="list-item">{x}</li>})}</ul> */}
-                <span className="list-el" onClick={() => setLine('local')}>Réseau local</span>
+                <span className={`list-el ${showLine === 'local' ? 'active' : ''}`} onClick={() => setLine('local')}>Réseau local</span>
                 <ul>{locals!.map((x, index) => {return showLine === 'local' ? <li key={index} className="list-item">{`${x.id} - ${x.name} (${x.direction})`}</li> : ''})}</ul>
-                <span className="list-el" onClick={() => setLine('night')}>Réseau de nuit</span>
+                <span className={`list-el ${showLine === 'night' ? 'active' : ''}`} onClick={() => setLine('night')}>Réseau de nuit</span>
                 <ul>{night?.map((x, index) => {return showLine === 'night' ? <li key={index} className="list-item">{`${x.id} - ${x.name} (${x.direction})`}</li> : ''})}</ul>
-                <span className="list-el" onClick={() => setLine('express')}>Réseau express</span>
+                <span className={`list-el ${showLine === 'express' ? 'active' : ''}`} onClick={() => setLine('express')}>Réseau express</span>
                 <ul>{express?.map((x, index) => {return showLine === 'express' ? <li key={index} className="list-item">{`${x.id} - ${x.name} (${x.direction})`}</li> : ''})}</ul>
-                <span className="list-el" onClick={() => setLine('shuttle')}>Navettes</span>
+                <span className={`list-el ${showLine === 'shuttle' ? 'active' : ''}`} onClick={() => setLine('shuttle')}>Navettes</span>
                 <ul>{shuttle?.map((x, index) => {return showLine === 'shuttle' ? <li key={index} className="list-item">{`${x.id} - ${x.name} (${x.direction})`}</li> : ''})}</ul>
-                <span className="list-el" onClick={() => setLine('shuttleOr')}>Navettes Or</span>
+                <span className={`list-el ${showLine === 'shuttleOr' ? 'active' : ''}`} onClick={() => setLine('shuttleOr')}>Navettes Or</span>
                 <ul>{shuttleOr?.map((x, index) => {return showLine === 'shuttleOr' ? <li key={index} className="list-item">{`${x.id} - ${x.name} (${x.direction})`}</li> : ''})}</ul>
               </div>
 
